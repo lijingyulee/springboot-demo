c6650a10d9d153b11ec8d5c2ec870824f7622d22
lijingyulee <lijingyulee@126.com>
2019-04-12 22:20:07 +0800

    异步回调

--- async/src/main/java/com/learn/async/controller/AsyncController.java
+++ async/src/main/java/com/learn/async/controller/AsyncController.java
@@ -4,8 +4,12 @@ import com.learn.async.event.TestEvent;
 import com.learn.async.service.AsyncService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @author lijy
@@ -45,6 +49,24 @@ public class AsyncController {
         publiser.publishEvent(new TestEvent(this, "测试事件监听"));
         for (int j = 0; j < 10; j++) {
             System.out.println("j = " + j);
+        }
+    }
+
+    @GetMapping("test-future")
+    public void testFuture() {
+        try {
+            Future<String> result1 = asyncService.firstTask();
+            Future<String> result2 = asyncService.secondTask();
+            Future<String> result3 = asyncService.thirdTask();
+            while (true) {
+                if (result1.isDone() && result2.isDone() && result3.isDone()) {
+                    System.out.println("执行异步回调");
+                    break;
+                }
+            }
+            System.out.println("异步回调结束");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
--- async/src/main/java/com/learn/async/service/AsyncService.java
+++ async/src/main/java/com/learn/async/service/AsyncService.java
@@ -1,7 +1,12 @@
 package com.learn.async.service;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.Future;
 
 /**
  * @author lijy
@@ -10,6 +15,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AsyncService {
+
+    public static Random random =new Random();
+
     @Async
     public void test() throws InterruptedException {
         Thread.sleep(2000);
@@ -17,4 +25,35 @@ public class AsyncService {
             System.out.println("i = " + i);
         }
     }
+
+    @Async
+    public Future<String> firstTask() throws InterruptedException {
+        System.out.println("开始做任务一");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务一，当前线程：" + Thread.currentThread().getName() + "，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务一完成");
+    }
+
+    @Async
+    public Future<String> secondTask() throws InterruptedException {
+        System.out.println("开始做任务二");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务二，当前线程：" + Thread.currentThread().getName() + "，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务二完成");
+    }
+
+    @Async
+    public Future<String> thirdTask() throws InterruptedException {
+        System.out.println("开始做任务三");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务三，当前线程：" + Thread.currentThread().getName() + "，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务三完成");
+    }
+
 }
