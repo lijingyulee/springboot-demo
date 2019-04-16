7524b85ebf37c1e8016872b982dad3862bbbeff8
lijy <lijingyulee@126.com>
2019-04-16 11:25:41 +0800

    fix

--- jredis/src/main/java/com/learn/jredis/util/RedisUtil2.java
+++ jredis/src/main/java/com/learn/jredis/util/RedisUtil2.java
@@ -442,7 +442,7 @@ public class RedisUtil2 {
      * 通过索引 获取list中的值
      *
      * @param key   键
-     * @param index 索引 index&gt;=0时， 0 表头，1 第二个元素，依次类推；index&lt;0时，-1，表尾，-2倒数第二个元素，依次类推
+     * @param index 索引 index>0时， 0 表头，1 第二个元素，依次类推；index<0时，-1，表尾，-2倒数第二个元素，依次类推
      * @return
      */
     public Object lGetIndex(String key, long index) {
