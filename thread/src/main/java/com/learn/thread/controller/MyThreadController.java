a2b5b2c7b70cb8b98fb74557d9fab5cd2500f9b1
lijingyulee <lijingyulee@126.com>
2019-04-23 22:53:14 +0800

    join

--- thread/src/main/java/com/learn/thread/controller/MyThreadController.java
+++ thread/src/main/java/com/learn/thread/controller/MyThreadController.java
@@ -3,6 +3,7 @@ package com.learn.thread.controller;
 import com.learn.thread.pojo.thread.MyThread;
 import com.learn.thread.pojo.thread.Mythread2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -38,6 +39,23 @@ public class MyThreadController {
             }
             System.out.println("end thread");
         }).start();
+    }
+
+    @RequestMapping("test-join")
+    public void testJoin() throws InterruptedException {
+        System.out.println("主线程start");
+        Thread thread = new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                System.out.println("i = " + i);
+            }
+        });
+        thread.start();
+        thread.join();
+        for (int j = 0; j < 100; j++) {
+            System.out.println("j = " + j);
+        }
+        System.out.println("主线程end");
+
     }
 
 }
