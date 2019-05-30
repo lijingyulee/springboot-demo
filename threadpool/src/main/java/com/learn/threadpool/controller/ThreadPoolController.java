a66397efa33c8dfecbe5b86e1c37b5c1198c1531
lijingyulee <lijingyulee@126.com>
2019-05-30 08:28:04 +0800

    semaphore amend

--- threadpool/src/main/java/com/learn/threadpool/controller/ThreadPoolController.java
+++ threadpool/src/main/java/com/learn/threadpool/controller/ThreadPoolController.java
@@ -2,11 +2,13 @@ package com.learn.threadpool.controller;
 
 import com.learn.threadpool.pojo.countdownlatch.Thread1;
 import com.learn.threadpool.pojo.countdownlatch.Thread2;
+import com.learn.threadpool.pojo.countdownlatch.semaphore.SemaphoreThread;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 
 /**
  * @author lijy
@@ -30,4 +32,13 @@ public class ThreadPoolController {
             e.printStackTrace();
         }
     }
+
+    @RequestMapping("test-semaphore")
+    public void testSemaphore() {
+        Semaphore semaphore = new Semaphore(2);
+        for (int i = 1; i <= 3; i++) {
+            new SemaphoreThread("第" + i + "个人", semaphore).start();
+        }
+    }
+    
 }
---
+++ threadpool/src/main/java/com/learn/threadpool/pojo/countdownlatch/semaphore/SemaphoreThread.java
@@ -0,0 +1,35 @@
+package com.learn.threadpool.pojo.countdownlatch.semaphore;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author lijy
+ * @create 2019-05-30 7:46
+ * @description
+ */
+public class SemaphoreThread extends Thread {
+    private String name;
+    private Semaphore semaphore;
+
+    public SemaphoreThread(String name, Semaphore semaphore) {
+        this.name = name;
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public void run() {
+        if (semaphore.availablePermits() <= 0) {
+            System.out.println(name + "等位中。。。");
+        }
+        try {
+            semaphore.acquire();
+            System.out.println(name + "开始就餐了。。");
+            Thread.sleep(new Random().nextInt(1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(name + "吃完了。。");
+        semaphore.release();
+    }
+}
